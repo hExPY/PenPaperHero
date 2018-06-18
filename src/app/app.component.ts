d7a5a9b7e8c2d7cbcefb7903bfb055d2f5c40bec
Marvyn Zalewski <mszalewski@ownpixel.com>
2018-06-18 20:29:25 +0200

    finalized navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 // import { ClipboardService } from 'ngx-clipboard';
 // import { NotificationsService } from 'angular2-notifications';
 import { StateService } from "./state.service";
+import { Router } from "@angular/router";
 
 
 @Component({
@@ -14,13 +15,19 @@ export class AppComponent implements OnInit {
   isNavbarCollapsed = true;
   public hero: any;
 
-  constructor(private state: StateService) { }
+  constructor(
+    private state: StateService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.state.loadHeroFromStorage();
     this.state.currentHero.subscribe(hero => {
       this.hero = hero
     })
+    this.router.events.subscribe(() => {
+      this.isNavbarCollapsed = true;
+    });
   }
 
   onFormChange(){
