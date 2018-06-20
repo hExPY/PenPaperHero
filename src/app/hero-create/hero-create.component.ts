b382f0f4890b68ce2de5eafecd99bbde29a9c14d
Marvyn Zalewski <mszalewski@ownpixel.com>
2018-06-20 19:13:10 +0200

    implemented image upload as avatar to creation view

--- src/app/hero-create/hero-create.component.ts
+++ src/app/hero-create/hero-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StateService } from "../state.service";
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-hero-create',
@@ -11,24 +12,44 @@ export class HeroCreateComponent implements OnInit {
   public hero: any;
   // public heroStateFormGroup: FormGroup;
   public model: any;
+  public heroAvatarUpload: File;
 
   constructor(
     private state: StateService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private sanitizer: DomSanitizer
   ) { }
 
   ngOnInit() {
     this.state.currentHero.subscribe(hero => {
       this.hero = hero
     });
+
     // this.heroStateFormGroup = this.formBuilder.group({
     //   model: "undef"
     // });
   }
 
+  onFileChanged(event) {
+    let reader = new FileReader();
+    if (event.target.files && event.target.files.length > 0) {
+      let file = event.target.files[0];
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.hero.details.avatar.type = file.type;
+        this.hero.details.avatar.data = reader.result.split(',')[1];
+        this.onFormChange();
+      };
+    }
+  }
+
   onHeroStateFormGroupChange(){
     // this.hero.details.state = this.heroStateFormGroup.value.model;
     this.onFormChange();
+  }
+
+  public getSantizeUrl() {
+    return this.sanitizer.bypassSecurityTrustUrl("data:" + this.hero.details.avatar.type + ";base64, " + this.hero.details.avatar.data);
   }
 
   onFormChange() {
